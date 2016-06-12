eeda9f96104b03c24c63bec7ed0ba4fd38a00b87
Sam Wilcoxon <samwilcoxon@gmail.com>
2016-06-12 22:52:33 +0200

    temp commit for color play

--- js/signup.js
+++ js/signup.js
@@ -2,52 +2,124 @@
 
 $(document).ready(function() {
 
-	var invalidSubmission = function () {
-		$('#landing-submit').hide();
-		$('#landing-error').show();
+	$('.floating-input-field input').on('change', function() {
+		if (this.value.trim()) {
+			$(this.parentNode).addClass('floating-input-label-up');
+		} else {
+			this.value = this.value.trim();
+			$(this.parentNode).removeClass('floating-input-label-up');
+		}
+	});
+
+	$('.floating-input-field input').on('keydown', function(e) {
+		// TODO check enter to submit
+
+	});
+
+	$('.input-toggle-option input').on('change', function() {
+		$('.input-toggle-option').removeClass('input-toggle-option-selected');
+		$(this.parentNode).addClass('input-toggle-option-selected');
+	});
+
+	var getBlend = function(r1, g1, b1, a1, r2, g2, b2) {
+		return {
+			r: Math.round(r2 + (r1-r2)*a1),
+			g: Math.round(g2 + (g1-g2)*a1),
+			b: Math.round(b2 + (b1-b2)*a1)
+		}
 	}
 
-	var resetSubmission = function () {
-		$('#landing-error').hide();
-		$('#landing-submit').show();
+	var body = document.body;
+	var heroSection = document.getElementById("header-hero");
+	var whoSection = document.getElementById("lorem-is-for");
+	var signupSection = document.getElementById("early-adopter");
+
+	var rgb1 = {r: 9, g: 21, b: 36};
+	var rgb2 = {r: 95, g: 5, b: 64};
+	var rgb3 = {r: 41, g: 210, b: 228};
+	var rgb4 = {r: 255, g: 255, b: 255};
+
+	var checkBg = function() {
+		var windowHeight = $(window).height();
+		var heroBottom = heroSection.getBoundingClientRect().bottom;
+		var whoBottom = whoSection.getBoundingClientRect().bottom;
+		var signupBottom = signupSection.getBoundingClientRect().bottom;
+
+		var resultObj;
+		if (heroBottom >= windowHeight) {
+			resultObj = rgb1;
+		} else if (heroBottom < windowHeight && heroBottom >= 0) {
+			// var step = 1/
+			var topOpacity = heroBottom/windowHeight;
+			var tempObj = getBlend(rgb1.r, rgb1.g, rgb1.b, topOpacity, 255, 255, 255);
+			resultObj = getBlend(rgb2.r, rgb2.g, rgb2.b, 1-topOpacity, tempObj.r, tempObj.g, tempObj.b);
+		} else if (whoBottom >= windowHeight) {
+			resultObj = rgb2;
+		} else if (whoBottom < windowHeight && whoBottom >= 0) {
+			var topOpacity = whoBottom/windowHeight;
+			var tempObj = getBlend(rgb2.r, rgb2.g, rgb2.b, topOpacity, 255, 255, 255);
+			resultObj = getBlend(rgb3.r, rgb3.g, rgb3.b, 1-topOpacity, tempObj.r, tempObj.g, tempObj.b);
+		} else if (signupBottom >= windowHeight) {
+			resultObj = rgb3;
+		} else if (signupBottom < windowHeight && signupBottom >= 0) {
+			var topOpacity = signupBottom/windowHeight;
+			var tempObj = getBlend(rgb3.r, rgb3.g, rgb3.b, topOpacity, 255, 255, 255);
+			resultObj = getBlend(rgb4.r, rgb4.g, rgb4.b, 1-topOpacity, tempObj.r, tempObj.g, tempObj.b);
+		} else {
+			resultObj = rgb4;
+		}
+		body.style.backgroundColor = 'rgb('+resultObj.r+', '+resultObj.g+', '+resultObj.b+')';
 	}
 
-	var successfulSubmission = function () {
-		resetSubmission();
-		$('#landing-info').hide();
-		$('#landing-form').hide();
-		$('#landing-success').show();
-	}
+	$(window).on('scroll resize', checkBg);
 
-	var submit = function () {
-		resetSubmission();
-		var emailAddr = ($('#landing-input-field').val() || "").trim();
-		if (!emailAddr || !$('#landing-input-field')[0].validity.valid) return invalidSubmission();
+
+	// var invalidSubmission = function () {
+	// 	$('#landing-submit').hide();
+	// 	$('#landing-error').show();
+	// }
+
+	// var resetSubmission = function () {
+	// 	$('#landing-error').hide();
+	// 	$('#landing-submit').show();
+	// }
+
+	// var successfulSubmission = function () {
+	// 	resetSubmission();
+	// 	$('#landing-info').hide();
+	// 	$('#landing-form').hide();
+	// 	$('#landing-success').show();
+	// }
+
+	// var submit = function () {
+	// 	resetSubmission();
+	// 	var emailAddr = ($('#landing-input-field').val() || "").trim();
+	// 	if (!emailAddr || !$('#landing-input-field')[0].validity.valid) return invalidSubmission();
 		
-		$.ajax({
-			url: "https://loremcms.us12.list-manage.com/subscribe/post-json?u=3b785e7fe49be8f02e4227a0d&amp;id=745fda5725",
-			data: $('#landing-form').serialize(),
-			dataType: "jsonp",
-			cache: false,
-			jsonp: "c",
-			contentType: "application/json; charset=utf-8",
-			success: successfulSubmission,
-			error: invalidSubmission
-		});
-	}
+	// 	$.ajax({
+	// 		url: "https://loremcms.us12.list-manage.com/subscribe/post-json?u=3b785e7fe49be8f02e4227a0d&amp;id=745fda5725",
+	// 		data: $('#landing-form').serialize(),
+	// 		dataType: "jsonp",
+	// 		cache: false,
+	// 		jsonp: "c",
+	// 		contentType: "application/json; charset=utf-8",
+	// 		success: successfulSubmission,
+	// 		error: invalidSubmission
+	// 	});
+	// }
 
-	$('#landing-form').submit(function (e) {
-		e.preventDefault();
-	})
+	// $('#landing-form').submit(function (e) {
+	// 	e.preventDefault();
+	// })
 
-	$('#landing-input-field').keyup(function(event) {
-		resetSubmission();
-		var key = event.keyCode || event.which;
-		if (key === 13) submit();
-	});
+	// $('#landing-input-field').keyup(function(event) {
+	// 	resetSubmission();
+	// 	var key = event.keyCode || event.which;
+	// 	if (key === 13) submit();
+	// });
 
-	$('.submit-btn').click(function() {
-		submit();
-	});
+	// $('.submit-btn').click(function() {
+	// 	submit();
+	// });
 
 });
