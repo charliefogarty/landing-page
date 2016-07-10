dc5fc2c8b7cb3fb9edea2c094a93006fec0d8983
Sam Wilcoxon <samwilcoxon@gmail.com>
2016-07-10 22:00:06 +0200

    add middle section

--- js/signup.js
+++ js/signup.js
@@ -86,14 +86,12 @@ $(document).ready(function() {
 	}
 
 	var emailQueryParam = setOrReadCookie('email', getQueryParamFromUrl(window.location.search, 'email')) || '';
-	var nameQueryParam = setOrReadCookie('name', getQueryParamFromUrl(window.location.search, 'name')) || '';
 	var typeQueryParam = setOrReadCookie('type', getQueryParamFromUrl(window.location.search, 'type')) || '';
 	var reffStr = readCookie('__reff') || '';
 	if (reffStr) checkAndSetReffCookie(reffStr);
 
 	// set the values in the form (hidden too)
 	if (emailQueryParam) document.getElementById('email-input').value = emailQueryParam;
-	if (nameQueryParam) document.getElementById('name-input').value = nameQueryParam;
 	if (typeQueryParam) {
 		var selectedToggle = $('input:radio[name="USERTYPE"][value="'+typeQueryParam+'"]')[0];
 		selectedToggle.setAttribute('checked', true);
@@ -113,7 +111,6 @@ $(document).ready(function() {
 
 	var successfulSubmission = function () {
 		createCookie('email',document.getElementById('email-input').value,100);
-		createCookie('name',document.getElementById('name-input').value,100);
 		createCookie('type',$('input:radio[name="USERTYPE"]:checked').val(),100);
 		
 		$('.landing-inputs').addClass('submitted');
